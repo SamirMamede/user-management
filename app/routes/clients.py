68a95f9b64f23218948a25be2f5af902b6a4f492
SamirMamede <anuarsamir@gmail.com>
2024-11-21 23:41:11 -0400

    Update clients routes.

--- app/routes/clients.py
+++ app/routes/clients.py
@@ -3,5 +3,17 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/clients")
-def helloworld():
+def get_clients():
+    return "Hello World"
+
+@router.post("/clients")
+def create_client():
+    return "Hello World"
+
+@router.put("/clients/{client_id}")
+def update_client(client_id: int):
+    return "Hello World"
+
+@router.delete("/clients/{client_id}")
+def delete_client(client_id: int):
     return "Hello World"
