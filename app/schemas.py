12f95240d9696084b68c31456d24b6af65d0218c
SamirMamede <anuarsamir@gmail.com>
2024-11-21 23:17:31 -0400

    Update clients schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,28 @@
 def clientEntity(client) -> dict:
     return { 
         "id": str(client["_id"]),
-        "name": client["name"],
+        "razao_social": client.get("razao_social"),
+        "nome_fantasia": client.get("nome_fantasia"),
+        "cnpj": client.get("cnpj"),
+        "proprietario": client.get("proprietario"),
+        "cpf_proprietario": client.get("cpf_proprietario"),
+        "rg_proprietario": client.get("rg_proprietario"),
+        "responsavel": client.get("responsavel"),
+        "cpf_responsavel": client.get("cpf_responsavel"),
+        "sistema_que_utiliza": client.get("sistema_que_utiliza"),
+        "mensalidade": client.get("mensalidade"),
+        "inscricao_estadual": client.get("inscricao_estadual"),
+        "inscricao_municipal": client.get("inscricao_municipal"),
+        "cnae_fiscal": client.get("cnae_fiscal"),
+        "regime_tributario": client.get("regime_tributario"),
+        "email": client.get("email"),
+        "celular": client.get("celular"),
+        "cep": client.get("cep"),
+        "municipio": client.get("municipio"),
+        "logradouro": client.get("logradouro"),
+        "numero": client.get("numero"),
+        "bairro": client.get("bairro"),
     }
+
+def clientsEntity(clients) -> list:
+    return [clientEntity(client) for client in clients]
