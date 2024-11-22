d150b0f4c19fa33c33478cbd45c773e6597403b7
SamirMamede <anuarsamir@gmail.com>
2024-11-21 23:31:37 -0400

    Add client models

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
+from typing import Optional
+
+class Client(BaseModel):
+    id: Optional[str]
+    razao_social: str
+    nome_fantasia: str
+    cnpj: str
+    proprietario: str
+    cpf_proprietario: str
+    rg_proprietario: str
+    responsavel: str
+    cpf_responsavel: str
+    sistema_que_utiliza: str
+    mensalidade: float
+    inscricao_estadual: Optional[str]
+    inscricao_municipal: Optional[str]
+    cnae_fiscal: Optional[str]
+    regime_tributario: str
+    email: EmailStr
+    celular: str
+    cep: str
+    municipio: str
+    logradouro: str
+    numero: str
+    bairro: str
